574b64e266fcd85aad9b7b16f421381c53b8ece1
Justin Dubs <jtdubs@gmail.com>
2019-11-22 07:50:17 -0500

    refactor to clean-up update.sh

--- update.sh
+++ update.sh
@@ -1,37 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-# Basic dot-files
-for F in .xsession .Xresources .gitconfig .dmrc .bashrc .bash_aliases
+FILES=$(find files/ -type f | sed 's/^files\///')
+UPDATES=
+
+for F in $FILES
 do
-  if [ $F -nt ~/$F ]
+  if [  files/$F -nt ~/$F ]
   then
     echo Updating $F...
-    cp -pf $F ~/$F
+    cp -pf files/$F ~/$F
+    UPDATES=$UPDATES:$F
   fi
 done
 
-# xmonad
-if [ .xmonad/xmonad.hs -nt ~/.xmonad/xmonad.hs ]
+if [[ $UPDATES =~ xmonad ]]
 then
-  echo Updating xmonad...
-  cp -pf .xmonad/xmonad.hs ~/.xmonad
-  xmonad --recompile && xmonad --restart
+  echo Restarting xmonad...
+  xmonad --recompile > /dev/null 2>&1
+  xmonad --restart   > /dev/null 2>&1
 fi
 
-# alacritty
-if [ .config/alacritty/alacritty.yml -nt ~/.config/alacritty/alacritty.yml ]
+if [[ $UPDATES =~ polybar ]]
 then
-  echo Updating alacritty...
-  cp -pf .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-fi
-
-# polybar
-if [ .config/polybar/config -nt ~/.config/polybar/config ]
-then
-  echo Updating polybar...
-  cp -pf .config/polybar/config ~/.config/polybar/config 
+  echo Restarting polybar...
 
   # kill polybar
   killall -q polybar || true
@@ -44,21 +37,9 @@ then
   polybar main > /dev/null 2>&1 &
 fi
 
-# nvim
-UPDATE_VIM=0
-for F in .config/nvim/init.vim .config/nvim/autoload/plug.vim .config/nvim/plugged/lightline.vim/autoload/lightline/colorscheme/seti.vim
-do
-  if [ $F -nt ~/$F ]
-  then
-    if [ $UPDATE_VIM -eq 0 ]
-    then
-      echo Updating NVim...
-      UPDATE_VIM=1
-    fi
-    cp -pf $F ~/$F
-  fi
-done
-if [ $UPDATE_VIM -eq 1 ]
+if [[ $UPDATES =~ nvim ]]
 then
+  echo Updating NVim...
   nvim --headless +PlugInstall +PlugUpdate +qa
 fi
+
