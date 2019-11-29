e5a5220ac2587ee5bc1e80c526cc12aec47e6fb2
Justin Dubs <jtdubs@gmail.com>
2019-11-29 12:08:02 -0500

    fix multi-display support

--- home/bin/switch_display.sh
+++ home/bin/switch_display.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # if current display is :0
-if grep -q :0 ~/bin/exec_display.sh
+if grep -q :0 ~/bin/source_display.sh
 then
     # and if Xephyr is running
     if pgrep Xephyr
     then
         # then be willing to switch to display :1
-        cp -f ~/bin/exec_display_1.sh ~/bin/exec_display.sh
+        echo export DISPLAY=:1 > ~/bin/source_display.sh
     fi
 # otherwise, if current is :1
 else
     # switch to :0
-    cp -f ~/bin/exec_display_0.sh ~/bin/exec_display.sh
+    echo export DISPLAY=:0 > ~/bin/source_display.sh
 fi
