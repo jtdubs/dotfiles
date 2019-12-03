d05994dd109e3a9696b0cef9fc2af97a3e851469
Justin Dubs <jtdubs@gmail.com>
2019-12-03 18:10:56 -0500

    change qutebrowser font to Noto

--- template/.config/qutebrowser/config.py
+++ template/.config/qutebrowser/config.py
@@ -114,3 +114,17 @@ c.colors.tabs.selected.even.bg = base0D
 # Default pages
 c.url.default_page = "https://duckduckgo.com/"
 c.url.start_pages = ["https://duckduckgo.com/"]
+
+# Fonts
+c.fonts.monospace = "Noto Sans Mono"
+c.fonts.completion.category = "bold 11pt monospace"
+c.fonts.completion.entry = "11pt monospace"
+c.fonts.debug_console = "11pt monospace"
+c.fonts.downloads = "11pt monospace"
+c.fonts.hints = "bold 11pt monospace"
+c.fonts.keyhint = "11pt monospace"
+c.fonts.messages.error = "11pt monospace"
+c.fonts.messages.info = "11pt monospace"
+c.fonts.messages.warning = "11pt monospace"
+c.fonts.statusbar = "11pt monospace"
+c.fonts.tabs = "11pt monospace"
