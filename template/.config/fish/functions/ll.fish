8a7dbabaf79e1d2a1872478e3e02ef32cff8a9c0
Justin Dubs <jtdubs@gmail.com>
2019-12-11 11:21:31 -0500

    tweaks to ll

--- template/.config/fish/functions/ll.fish
+++ template/.config/fish/functions/ll.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function ll --description 'alias ll=exa -lbghm --time-style long-iso --git'
-	exa -lbghm --time-style long-iso --git $argv;
+function ll --description 'alias ll=exa -lbgmF --time-style long-iso --git'
+	exa -lbgmF --time-style long-iso --git $argv;
 end
