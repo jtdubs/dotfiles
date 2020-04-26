05f18faf8e9d919bceb9fbce734499a116004b64
Justin Dubs <jtdubs@gmail.com>
2020-04-25 21:28:35 -0400

    set audio to HDMI if possible, otherwise internal

--- template/bin/auto_monitor.sh
+++ template/bin/auto_monitor.sh
@@ -8,5 +8,8 @@ else
     echo Auto-detecting monitor setup...
     autorandr -c
     sleep 2
+    bspc desktop any -m primary
+    bspc monitor -d 1 2 3 4 5 6 7 8 9 0
     feh --auto-zoom --bg-fill ~/.local/share/backgrounds/hardy_blue.png
+    pactl set-card-profile 0 output:hdmi-stereo-extra1 || pactl set-card-profile 0 output:analog-surround-40
 fi
