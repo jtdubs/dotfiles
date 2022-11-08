0d0e0c98f1f2692625957fa82ad076c8778583da
Justin Dubs <jtdubs@gmail.com>
2022-11-08 10:07:51 -0500

    fix nested linking on re-run

--- install.sh
+++ install.sh
@@ -2,6 +2,24 @@
 
 set -eEuo pipefail
 
+PACKAGES=(
+    bazel
+    build-essential
+    code
+    curl
+    git
+    gnome-tweaks
+    google-chrome-stable
+    keepassxc
+    neovim
+    nmap
+    python3
+    python3-pip
+    python3-venv
+    tmux
+    zsh
+)
+
 LINKS=(
     ".bash_aliases"
     ".bash_login"
@@ -9,6 +27,7 @@ LINKS=(
     ".config/alacritty"
     ".config/nvim"
     ".config/starship.toml"
+    ".config/systemd/user/gdrive.service"
     ".gitconfig"
     ".profile"
     ".ssh/environment"
@@ -18,8 +37,38 @@ LINKS=(
     ".zshtheme"
 )
 
+# echo "Installing packages..."
+# export DEBIAN_FRONTEND=noninteractive 
+# sudo apt-get -qq update
+# for p in ${PACKAGES[@]}; do
+#     # Check if package is already installed
+#     if [ $(apt list -qq --installed $p 2>/dev/null | wc -l) -ne 1 ]; then
+#         sudo apt-get install -y $p
+#     fi
+# done
+# echo "Done."
+
+if command -v go >/dev/null; then
+    echo "Install golang tools..."
+    if [ ! -e ~/go/bin/gopls ]; then
+        go install golang.org/x/tools/gopls@latest
+    fi
+    if [ ! -e ~/go/bin/dlv ]; then
+        go install github.com/go-delve/delve/cmd/dlv@latest
+    fi
+    if [ ! -e ~/go/bin/staticcheck ]; then
+        go install honnef.co/go/tools/cmd/staticcheck@latest
+    fi
+    if [ ! -e ~/go/bin/rclone ]; then
+        go install github.com/rclone/rclone@latest
+    fi
+    echo "Done."
+fi
+
 echo "Creating symlinks..."
 for l in ${LINKS[@]}; do
-    ln -sf $PWD/home/$l $HOME/$l
+    if ! readlink $HOME/$l; then
+        ln -sf $PWD/home/$l $HOME/$l
+    fi
 done
 echo "Done."
